58088653e9aedd500f4e23197b5cd4465a9964a6
Brittney Fortier <brittfort34@gmail.com>
2019-05-02 13:35:03 -0400

    A TDD approach to a program that checks a DateTime to see if it is a holiday or weekend day.

---
+++ BillPayTdd/BillPayTdd/BillDueDateUnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BillPayTdd
+{
+    [TestFixture]
+    class BillDueDateUnitTest1
+    {
+        [Test]
+        public void ifBussinessDay_ReturnDueDate()
+        {
+            DateTime input = new DateTime(2018, 8, 6);
+            var mockHolidayService = new HolidayService();
+            var _bill = new Bill(mockHolidayService);
+            var output = _bill.CheckDate(input);
+            var expected = input;
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ifSaturday_ReturnMonday()
+        {
+            DateTime input = new DateTime(2019, 5, 4);
+            var mockHolidayService = new HolidayService();
+            var _bill = new Bill(mockHolidayService);
+            var output = _bill.CheckDate(input);
+            var expected = new DateTime(2019, 5, 6);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ifSunday_ReturnMonday()
+        {
+            DateTime input = new DateTime(2019, 5, 5);
+            var mockHolidayService = new HolidayService();
+            var _bill = new Bill(mockHolidayService);
+            var output = _bill.CheckDate(input);
+            var expected = new DateTime(2019, 5, 6);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ifHoliday_ReturnNonHoliday()
+        {
+            DateTime input = new DateTime(2018, 8, 3); // Friday
+            var mockHolidayService = new HolidayService();
+            var _bill = new Bill(mockHolidayService);
+            var output = _bill.CheckDate(input);
+            var expected = new DateTime(2018, 8, 6);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ifHoliday_ReturnWeekday()
+        {
+            DateTime input = new DateTime(2018, 8, 3); // Tuesday 12/15  or Friday 8/3
+            var mockHolidayService = new HolidayService();
+            var _bill = new Bill(mockHolidayService);
+            var output = _bill.CheckDate(input);
+            var expected = new DateTime(2018, 8, 6);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
---
+++ BillPayTdd/HolidayService.cs/Bill.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillPayTdd
+{
+    public class Bill
+    {
+        HolidayService Holiday;
+        //bring in holiday service to check for holidays
+        //use dependency injection
+
+        public Bill(HolidayService holiday)
+        {
+            this.Holiday = holiday;
+        }
+
+        public DateTime CheckDate(DateTime dueDate)
+        {
+            if (dueDate.DayOfWeek == DayOfWeek.Saturday)
+                return dueDate.AddDays(2);
+            if (dueDate.DayOfWeek == DayOfWeek.Sunday)
+                return dueDate.AddDays(1);
+            if (Holiday.isHoliday(dueDate))
+            {
+                if (dueDate.DayOfWeek != DayOfWeek.Friday)
+                    return dueDate.AddDays(1);
+                else
+                    return dueDate.AddDays(3);
+            }
+            return dueDate;
+        }
+    }
+}
