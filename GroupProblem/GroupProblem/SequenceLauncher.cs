32bd7420ddbd1a78366d4d8b86437d9c88665a61
Brittney Fortier <brittfort34@gmail.com>
2019-05-11 12:45:05 -0400

    TDD approach to finding the smallest number in an int array.

---
+++ GroupProblem/GroupProblem/SequenceLauncher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProblem
+{
+    public class SequenceLauncher
+    {
+        public bool LaunchSequenceChecker(string[] systemNames, int[] stepNumbers)
+        {
+            for (int i = 0; i < stepNumbers.Length; i++)
+            {
+                List<string> systemNamesList = new List<string>();
+                if (stepNumbers[i] > stepNumbers[i + 1])
+                    return false;
+              
+            }
+
+            return true;
+        }
+    }  
+}
---
+++ GroupProblem/Tests/UnitTests.cs
@@ -0,0 +1,57 @@
+﻿using GroupProblem;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void IfEmptyArray_ReturnTrue()
+        {
+            string[] test_names = new string[] { };
+            int[] test_numbers = new int[] { };
+            SequenceLauncher _sequenceLauncher = new SequenceLauncher();
+            var actual = _sequenceLauncher.LaunchSequenceChecker(test_names, test_numbers);
+
+            Assert.AreEqual(actual, true);
+        }
+
+        [Test]
+        public void IfUnordered_ReturnFalse()
+        {
+            string[] test_names = new string[] { "stage1", "stage1"};
+            int[] test_numbers = new int[] { 2, 1 };
+            SequenceLauncher _sequenceLauncher = new SequenceLauncher();
+            var actual = _sequenceLauncher.LaunchSequenceChecker(test_names, test_numbers);
+            Assert.AreEqual(actual, false);
+        }
+
+        [Test]
+        public void IfOrdered_ReturnTrue()
+        {
+            string[] test_names = new string[] { "stage1", "stage1" };
+            int[] test_numbers = new int[] { 1, 2 };
+            SequenceLauncher _sequenceLauncher = new SequenceLauncher();
+            var actual = _sequenceLauncher.LaunchSequenceChecker(test_names, test_numbers);
+            Assert.AreEqual(actual, true);
+        }
+
+        [Test]
+        public void CheckMultiSystemNamesForDesOrder()
+        {
+            string[] test_names = new string[] { "stage1", "stage1", "stage2" };
+            int[] test_numbers = new int[] { 2, 1, 5 };
+            SequenceLauncher _sequenceLauncher = new SequenceLauncher();
+            var actual = _sequenceLauncher.LaunchSequenceChecker(test_names, test_numbers);
+            Assert.AreEqual(actual, false);
+        }
+
+
+    }
+}
