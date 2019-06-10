b628bdb5f553cde46a519ba3bc7a131b3cb5ae45
Brittney Fortier <brittfort34@gmail.com>
2019-06-10 13:00:05 -0400

    The logic behind winning a game of Rock, Paper, Scissors.

---
+++ RockPaperScissors/RockPaperScissors/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RockPaperScissors
+{
+    public class Game 
+    {
+      
+        public static Gesture DetermineWhoWins(Gesture player1, Gesture player2)
+        {
+            Gesture Rock = Gesture.Rock;
+            Gesture Paper = Gesture.Paper;
+            Gesture Scissor = Gesture.Scissor;
+            Gesture Draw = Gesture.Draw;
+
+            if (player1 == Rock && player2 == Scissor || player2 == Rock && player1 == Scissor)
+                return Rock;
+            if (player1 == Scissor && player2 == Paper || player2 == Scissor && player1 == Paper)
+                return Scissor;
+            if (player1 == Rock && player2 == Paper || player2 == Rock && player1 == Paper)
+                return Paper;
+
+            return Draw;
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors/Gesture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public enum Gesture
+    {
+        Rock,
+        Paper,
+        Scissor,
+        Draw
+    }
+}
