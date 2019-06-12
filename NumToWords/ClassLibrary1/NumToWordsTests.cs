ee33b7c4a1375e32a009d53f03b52b8959167079
Brittney Fortier <brittfort34@gmail.com>
2019-06-11 22:19:17 -0400

    A program to convert a number of 4 digits or less, into the word equivalent.

---
+++ NumToWords/ClassLibrary1/NumToWordsTests.cs
@@ -0,0 +1,64 @@
+﻿using NumToWords;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class NumToWordsTests
+    {
+        // public Converter _convert = new Converter();
+        int num;
+
+        [Test]
+        public void ReturnNumberOfDigitsInNumber_ThreeDigits_ShouldReturnThree()
+        {
+            num = 100;
+
+            int expected = 3;
+            int actual = Converter.ReturnNumberOfDigitsInNum(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNumberIs0_ReturnZero()
+        {
+            num = 0;
+
+            string expected = "zero";
+            string actual = Converter.ConvertFromNumToWords(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNumberIs2_ReturnTwo()
+        {
+            num = 2;
+
+            string expected = "two";
+            string actual = Converter.ConvertFromNumToWords(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void IfNumberIs8_ReturnEight()
+        {
+            num = 8;
+
+            string expected = "eight";
+            string actual = Converter.ConvertFromNumToWords(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }           
+}
---
+++ NumToWords/NumToWords/Converter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumToWords
+{
+    public class Converter
+    {
+        
+
+        public static string ConvertFromNumToWords(int numToConvert)
+        {
+            String wordEquiv = "";
+
+            Dictionary<int, string> singleDigits = new Dictionary<int, string>
+            {
+                [0] = "zero",
+                [1] = "one",
+                [2] = "two",
+                [3] = "three",
+                [4] = "four",
+                [5] = "five",
+                [6] = "six",
+                [7] = "seven",
+                [8] = "eight",
+                [9] = "nine"
+            };
+
+            if (singleDigits.ContainsKey(numToConvert))
+                wordEquiv = singleDigits[numToConvert];
+
+            return wordEquiv;
+        }
+
+        public static int ReturnNumberOfDigitsInNum(int number)
+        {
+            return number.ToString().Length;
+        }
+    }
+}
